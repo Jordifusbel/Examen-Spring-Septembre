7e7e29e56dd3b81409e990f02f8783f3d338816d
Jordi <32875964+Jordifusbel@users.noreply.github.com>
2018-09-05 17:46:31 +0200

    Creado mapeo para clase persona

--- 17_setmana_4_exercici/src/main/java/org/formacio/domain/Persona.java
+++ 17_setmana_4_exercici/src/main/java/org/formacio/domain/Persona.java
@@ -3,14 +3,24 @@ package org.formacio.domain;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 
+@SequenceGenerator(name="sequencia_PERSONA", sequenceName="SEQ_PERSONA")
 public class Persona {
 
 	/*
 	 * Persona ha d'emprar la sequencia SEQ_PERSONA per els ids
 	 */
+	
+	@Id @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="sequencia_PERSONA")
+	@Column(name="per_id")
 	private Long id;
 	
+	@Column(name="per_nom")
 	private String nom;
 	
 	private Set<Casa> propietats = new HashSet<>();
